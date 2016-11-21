71b4cf06284a0c159c23bcca7d341f3e005c6821
tomdach <tomdach16@gmail.com>
2016-11-21 16:43:14 +0100

    ajoute fichier

---
+++ chall1.js
@@ -0,0 +1,52 @@
+alert("étes vous prés?")
+alert("a voir un texte debile?")
+alert("dans ")
+alert("3")
+alert("2")
+alert("1")
+alert("page not found 404")
+alert("page not found 404")
+alert("page not found 404")
+alert("page not found 404")
+alert("page not found 404")
+alert("page not found 404")
+alert("page not found 404")
+alert("page not found 404")
+alert("page not found 404")
+alert("je vais arreter")
+var a = prompt ("Entrez votre nom");
+var b = document.getElementById('nom');
+b.innerHTML = a;
+
+var c = prompt ("Entrez votre age")
+var d = document.getElementById("age");
+d.innerHTML = c;
+
+
+var e = prompt ("Entrez le nom d'un monstre (un peux wtf)");
+var f = document.getElementById("monstre");
+f.innerHTML = e;
+
+var g = prompt ("Entrez le nom d'une ville");
+var h = document.getElementById("ville");
+h.innerHTML = g;
+
+var i = prompt ("Entrez la météo");
+var j = document.getElementById("temps");
+j.innerHTML = i;
+
+var k = prompt ("Entrez le nom d'un animal (metté un truc wtf)");
+var l = document.getElementById("animaux");
+l.innerHTML = k;
+
+var m = prompt ("Entrez le nom d'un aliments");
+var n = document.getElementById("aliments");
+n.innerHTML = m;
+
+var o = prompt ("Entrez le nom d'une action que un couple peux faire...");
+var p = document.getElementById("action");
+p.innerHTML = o;
+
+debut.style.visibility = "visible";
+
+
